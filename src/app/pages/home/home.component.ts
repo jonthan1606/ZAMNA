d1631464e63b96c2a34919f8853011a314db7585
Angel Daniel <116531997+DanielCodexp@users.noreply.github.com>
2024-11-18 23:29:32 -0600

    SEO, metaTitle, metaDescription

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  constructor(
+    public titleService: Title,
+    public meta: Meta,
+  ) { }
+
+  async ngOnInit() {
+    this.titleService.setTitle('Zamna13 | Innovación Tecnológica');
+    this.meta.updateTag({ property: 'og:image', content: 'https://zamna13.com/logos/logo-black.jpg' });
+    this.meta.updateTag({ name: 'description', content: "Zamna13 es una empresa de innovación tecnológica que se dedica a la creación de soluciones tecnológicas para empresas y emprendedores." });
+  }
 }
